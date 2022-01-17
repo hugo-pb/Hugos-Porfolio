2ac662c856437d59610b3623aef8de2be95644a4
hugo-pb <h.palomerabecerra@gmail.com>
2022-01-16 21:32:43 -0700

    added some styler changes

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+      <div Footer>
+        <p>
+          This page was made by <a href="https://github.com/hugo-pb">Hugo</a>
+        </p>
+      </div>
+    );
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,21 @@
 import React from "react";
-
+import "../styles/Projects.css"
 export default function Projects() {
   return (
     <div className="Projects">
       <div className="row">
         {" "}
-        <h1>Projects</h1>
+        <h1 className="Project">Projects</h1>
       </div>
       <div className="row">
         <div className="col-6">
           <h1>
             {" "}
-            <a href="https://61ddaf775671144dbcfa7b83--vibrant-mestorf-8ee54c.netlify.app/">
+            <a
+              href="https://61ddaf775671144dbcfa7b83--vibrant-mestorf-8ee54c.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
               React-Weather
             </a>{" "}
           </h1>
@@ -22,9 +26,10 @@ export default function Projects() {
         </div>
         <div className="col-6">
           <h1>
-            <a href="https://hugojocelyn.com">Wedding Website </a>
+            <a href="https://hugojocelyn.com"  target="_blank"
+              rel="noreferrer">Wedding Website </a>
           </h1>
-          <p>Real website used for a real Wedding hugojocelyn.com</p>
+          <p>While we were learning to code we desided to put in to practice this new skills so we develope our own webside for a very special ocation, using vanilla html, css, js, and php hugojocelyn.com</p>
         </div>
       </div>
     </div>
