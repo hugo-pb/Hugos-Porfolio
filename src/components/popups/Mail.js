a97c3de77587be9ec80887a1733840d51a4245df
hugo-pb <h.palomerabecerra@gmail.com>
2022-01-24 12:16:21 -0700

    emsil.jd

--- src/components/popups/Mail.js
+++ src/components/popups/Mail.js
@@ -4,12 +4,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "react-bootstrap/Button";
 import Overlay from "react-bootstrap/Overlay";
 import Tooltip from "react-bootstrap/Tooltip";
-import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 export default function Mail() {
   const [show, setShow] = useState(false);
   const target = useRef(null);
 
+  const sendEmail = (e) => {
+    e.preventDefault();
 
+    emailjs
+      .sendForm(
+        "service_w0o5sxk",
+        "template_9tp1wc8",
+        e.target,
+        "user_9a1xFu4TkjIjEATG3yLDW"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
 
   return (
     <div>
@@ -19,10 +38,10 @@ export default function Mail() {
       <Overlay target={target.current} show={show} placement="right">
         {(props) => (
           <Tooltip id="overlay-Mail" {...props}>
-            <form id="contact-form" >
-              <input type="text"  placeholder="Name" />
+            <form id="contact-form" onSubmit={sendEmail} method="post">
+              <input type="text" placeholder="Name" name="name" />
               <br />
-              <input type="email" name="user_email" placeholder="Email" />
+              <input type="email"  placeholder="Email" name="email" />
               <br />
               <textarea name="message" placeholder="Message" />
               <br />
